4f1288cc88be57929853e77130de756281f69bc8
Hans Wang <hanswang123456@gmail.com>
2023-07-09 12:29:16 -0400

    redid to server_port

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
   res.send("first request");
 });
 
-app.listen(WEBSITE_PORT, () => {
+app.listen(SERVER_PORT, () => {
   connect();
   console.log("Listening on 8080");
 });
