d5d8dae8fc905ec421b1a4bd1cf43c6420b07861
Hans Wang <hanswang123456@gmail.com>
2023-07-09 10:23:38 -0400

    updated to variable WEBSITE_PORT

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
   res.send("first request");
 });
 
-app.listen(process.env.PORT || 8080, () => {
+app.listen(WEBSITE_PORT, () => {
   connect();
   console.log("Listening on 8080");
 });
